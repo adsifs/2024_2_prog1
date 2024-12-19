aee525ca799731eda8fdc6a24023b3604d4c8da1
Nicholas Amarante <nicholasfelipe361@gmail.com>
2024-12-19 20:51:14 -0300

    atualizando Cargo

--- projeto/barbearia/src/main/java/br/ifs/cads/barbearia/model/Cargo.java
+++ projeto/barbearia/src/main/java/br/ifs/cads/barbearia/model/Cargo.java
@@ -1,6 +1,7 @@
 package br.ifs.cads.barbearia.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 
 import java.util.List;
 
@@ -8,6 +9,8 @@ import java.util.List;
 public class Cargo extends AuditModel{
 
     private String nome;
+    @OneToMany(mappedBy = "cargoId")
+    private List<Funcionario> funcionarios;
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
 }
