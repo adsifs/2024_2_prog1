5c36c51b5a791dbc669ed35ad2faccf009ca7cb1
Francisco Rodrigues <frchico@users.noreply.github.com>
2025-02-20 19:33:05 -0300

    Correção do nome da classe

--- projeto/barbearia/src/main/java/br/ifs/cads/barbearia/repository/ClienteRepository.java
+++ projeto/barbearia/src/main/java/br/ifs/cads/barbearia/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package br.ifs.cads.barbearia.repository;
 import br.ifs.cads.barbearia.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteItemRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
