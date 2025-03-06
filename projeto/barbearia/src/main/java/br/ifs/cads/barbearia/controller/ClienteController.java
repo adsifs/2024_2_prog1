f7287cb5ba4356472207e5d2a39d2ba0e437f3d4
Francisco Rodrigues <frchico@gmail.com>
2025-03-06 20:18:22 -0300

    Ajuste de imports e correções de pequenos erros.

--- projeto/barbearia/src/main/java/br/ifs/cads/barbearia/controller/ClienteController.java
+++ projeto/barbearia/src/main/java/br/ifs/cads/barbearia/controller/ClienteController.java
@@ -2,11 +2,12 @@ package br.ifs.cads.barbearia.controller;
 
 import br.ifs.cads.barbearia.model.Cliente;
 import br.ifs.cads.barbearia.repository.ClienteRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ClienteController {
--- projeto/barbearia/src/main/java/br/ifs/cads/barbearia/repository/AgendamentoRepository.java
+++ projeto/barbearia/src/main/java/br/ifs/cads/barbearia/repository/AgendamentoRepository.java
@@ -3,6 +3,9 @@ package br.ifs.cads.barbearia.repository;
 import br.ifs.cads.barbearia.model.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
     List<Agendamento> findByDataAgendamentoBetween(LocalDateTime start, LocalDateTime end);
 }
