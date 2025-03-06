eab35e8765d5b4d796c1d660324ddca6cfafeedc
matheusantoniogithub <155587692+matheusantoniogithub@users.noreply.github.com>
2025-03-06 20:06:27 -0300

    commit 06/03

--- projeto/barbearia/src/main/java/br/ifs/cads/barbearia/controller/ClienteController.java
+++ projeto/barbearia/src/main/java/br/ifs/cads/barbearia/controller/ClienteController.java
@@ -18,4 +18,44 @@ public class ClienteController {
     public Page<Cliente> getAllClientes(Pageable pageable) {
         return clienteRepository.findAll(pageable);
     }
+
+    // adicionar 
+    @PostMapping("/clientes")
+    public Cliente createCliente(@Valid @RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    // visualizar
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // editar
+    @PutMapping("/clientes/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @Valid @RequestBody Cliente clienteAtualizado) {
+        return clienteRepository.findById(id)
+                .map(cliente -> {
+                    cliente.setNome(clienteAtualizado.getNome());
+                    cliente.setEmail(clienteAtualizado.getEmail());
+                    cliente.setTelefone(clienteAtualizado.getTelefone());
+                    cliente.setBarbeariaFavorita(clienteAtualizado.getBarbeariaFavorita());
+                    Cliente atualizado = clienteRepository.save(cliente);
+                    return ResponseEntity.ok(atualizado);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // remover um cliente
+    @DeleteMapping("/clientes/{id}")
+    public ResponseEntity<?> deleteCliente(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .map(cliente -> {
+                    clienteRepository.delete(cliente);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
